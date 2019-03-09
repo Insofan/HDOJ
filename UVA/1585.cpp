e33f8e9b2cfed6c6628dcb657f5c3e80898d7da8
Insomnia <282132315@qq.com>
2019-03-10 01:53:09 +0800

    uva 1585

---
+++ UVA/1585.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Insomnia on 19-3-10.
+// MIT License
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int get(int cnt) {
+    int sum = 0;
+    while (cnt) {
+       sum += cnt--;
+    }
+
+    return sum;
+}
+int main() {
+    int cnt;
+    cin >> cnt;
+    string s;
+
+
+        while (cin >> s) {
+        int score = 0;
+        int numCnt = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            if (s[j] == 'O') {
+              numCnt++;
+              if (j == s.length() - 1) {
+               score += get(numCnt);
+              }
+            } else if (s[j] == 'X') {
+              score += get(numCnt);
+              numCnt = 0;
+            }
+        }
+        cout << score << endl;
+        cnt--;
+        if (cnt == 0) {
+            break;
+        }
+        }
+
+    return 0;
+}
