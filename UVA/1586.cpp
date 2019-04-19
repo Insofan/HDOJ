58f35c1d07d96d4a3ce75997e63abc0c983b33bd
Insofan <282132315@qq.com>
2019-04-19 09:08:35 +0800

    uva 1586

--- UVA/1586.cpp
+++ UVA/1586.cpp
@@ -7,43 +7,44 @@
 
 #include <iostream>
 #include <cstring>
+#include <map>
 using namespace std;
 #define maxn 100
 
-double getVal(char tmp) {
-    switch (tmp) {
-        case 'C':
-            return 12.01;
-        case 'H':
-            return 1.008;
-        case 'O':
-            return 16.00;
-        case 'N':
-            return 14.01;
-    }
-    return 0.0;
-}
 
 int main() {
+    map<char, double> valMap;
+    valMap['C'] = 12.01;
+    valMap['H'] = 1.008;
+    valMap['O'] = 16.00;
+    valMap['N'] = 14.01;
 
     int k;
     cin >> k;
     while (k--) {
         char s[maxn];
         scanf("%s", s);
-        int n = strlen(s);
         double res = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (isdigit(s[i])) {
-                int mul = 0;
-                while (isdigit(s[i])) {
-                    mul = mul * 10 + (s[i--] - '0');
-                }
-                res += mul * getVal(s[--i]);
-            } else {
-                res += getVal(s[i]);
-            }
+        int len = strlen(s);
+
+        s[len] = 'E';
+
+        for (int i = 0; s[i] != 'E'; i++) {
+           if (valMap[s[i]]) {
+               if (s[i + 1] < '0' || s[i + 1] > '9') {
+                   res += valMap[s[i]];
+               } else {
+                   int base = 0, cur = i + 1;
+
+                   while (isdigit(s[cur])) {
+                       base = base * 10 + s[cur++] - '0';
+                   }
+
+                   res += valMap[s[i]] * base;
+               }
+           }
         }
+
         printf("%.3f\n", res);
     }
     return 0;
